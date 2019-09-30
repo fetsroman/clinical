026edd6b9f8fd499455fadee4f556a474be72b98
fetsroman <fets.roman@gmail.com>
2019-09-30 16:35:07 +0300

    Create Address

--- db/schema.rb
+++ db/schema.rb
@@ -10,10 +10,16 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2019_09_30_131940) do
+ActiveRecord::Schema.define(version: 2019_09_30_132400) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
+
+  create_table "addresses", force: :cascade do |t|
+    t.string "address"
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+  end
 
   create_table "jwt_blacklists", force: :cascade do |t|
     t.string "jti"
