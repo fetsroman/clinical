e7827bed864af6af9b6938922249f6b735ab6d7a
fetsroman <fets.roman@gmail.com>
2019-10-08 12:14:41 +0300

    Telegram Bot fix

--- app/services/telegram_bot.rb
+++ app/services/telegram_bot.rb
@@ -5,12 +5,10 @@ token = ENV['TELEGRAM_BOT_API_TOKEN']
 Telegram::Bot::Client.run("911783530:AAHM1AsQryiOQHbpAe7yRehMA0plfVMP0Zs") do |bot|
   bot.listen do |message|
     case message.text
-    when 'e'
-      bot.api.send_message(chat_id: 440375095, text: "Hello")
     when '/start'
-      bot.api.send_message(chat_id: 440375095, text: "Hello, #{message.from.first_name}")
+      bot.api.send_message(chat_id: message.chat.id, text: "Hello, #{message.from.first_name}")
     when '/stop'
-      bot.api.send_message(chat_id: 440375095, text: "Bye, #{message.from.first_name}")
+      bot.api.send_message(chat_id: message.chat.id, text: "BB, #{message.from.first_name}")
     end
   end
 end
