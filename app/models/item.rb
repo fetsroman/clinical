55cbac0200dad7b307c83b5d758aeb89f9b7e8c8
fetsroman <fets.roman@gmail.com>
2019-11-22 17:11:41 +0200

    Dependent destroy to Items

--- app/models/item.rb
+++ app/models/item.rb
@@ -2,7 +2,7 @@ class Item < ApplicationRecord
   include PgSearch::Model
 
   belongs_to :category, optional: true
-  has_many :options
+  has_many :options, dependent: :destroy
   has_many :line_items, dependent: :destroy
   accepts_nested_attributes_for :options, reject_if: :all_blank, allow_destroy: true
 
