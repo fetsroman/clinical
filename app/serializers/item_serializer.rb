b9ec6a95bfa707368b5ace89f182bea7e3aa00d4
roman <fets.roman@gmail.com>
2020-01-30 16:43:40 +0200

    Fix order option 500

--- app/serializers/item_serializer.rb
+++ app/serializers/item_serializer.rb
@@ -1,17 +1,12 @@
 class ItemSerializer < ActiveModel::Serializer
   attributes :id, :title, :description, :image, :options
   # has_one :category
-  # has_many :options
-
+  has_many :options, -> { order(:article) }
   def description
     if current_user.country == "Україна"
       self.object.description_uk
     elsif current_user.country == "Россия"
       self.object.description_ru
     end
-  end
-
-  def options
-    OptionSerializer.new(object.options.order("article"))
   end
 end
